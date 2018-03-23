b15d2c9da5ec0f864a77a3541f7eac6e41dd4144
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2018-03-23 16:32:30 +0100

    Fix Arquillian
    * Changed some configuration to fix the build

--- TACOS-persistence/src/test/java/cz/fi/muni/TACOS/persistence/entity/UserTest.java
+++ TACOS-persistence/src/test/java/cz/fi/muni/TACOS/persistence/entity/UserTest.java
@@ -4,18 +4,12 @@ import cz.fi.muni.TACOS.persistence.enums.UserRole;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ArchivePath;
 import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import javax.inject.Inject;
-import javax.jws.soap.SOAPBinding;
 
 
 @RunWith(Arquillian.class)
@@ -24,20 +18,21 @@ public class UserTest {
 
     @Deployment
     public static Archive<?> createDeployment() {
-        return ShrinkWrap.create(WebArchive.class)
-                .addClasses(User.class)
+        return ShrinkWrap
+                .create(WebArchive.class)
+                .addPackages(true, "cz.fi.muni.TACOS.persistence")
                 .addAsManifestResource("test-persistence.xml", ArchivePaths.create("persistence.xml"));
     }
 
-    @Inject
-    User user;
-
+    private User setUpUserPavel() {
+        return new User("Pavel", "Vyskocil", "pa.vy@gmail.com", UserRole.SUPERADMIN);
+    }
 
     @Test
-    public void getName() throws Exception{
-        //User user = new User("Pavel", "sss", "sss", UserRole.SUPERADMIN);
+    public void getName() throws Exception {
+        User user = setUpUserPavel();
+
         user.setName("Pavel");
         Assert.assertEquals("Pavel", user.getName());
     }
-
 }
